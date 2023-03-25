1ab2ce3900f2462ea03bd65e7c16456e2b27c468
aljorgevi <jormencar@yahoo.com>
2023-03-25 12:54:22 +0000

    update whileHover

--- src/components/Toppings.tsx
+++ src/components/Toppings.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion as m } from 'framer-motion'
 
 const Toppings = ({ addTopping, pizza }) => {
 	const toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes']
@@ -11,15 +11,35 @@ const Toppings = ({ addTopping, pizza }) => {
 				{toppings.map(topping => {
 					const spanClass = pizza.toppings.includes(topping) ? 'active' : ''
 					return (
-						<li key={topping} onClick={() => addTopping(topping)}>
+						<m.li
+							key={topping}
+							onClick={() => addTopping(topping)}
+							whileHover={{
+								scale: 1.3,
+								color: '#f8e112',
+								originX: 0
+							}}
+							transition={{
+								type: 'spring',
+								stiffness: 300
+							}}
+						>
 							<span className={spanClass}>{topping}</span>
-						</li>
+						</m.li>
 					)
 				})}
 			</ul>
 
 			<Link to='/order'>
-				<button>Order</button>
+				<m.button
+					whileHover={{
+						scale: 1.1,
+						textShadow: '0px 0px 8px rgb(255,255,255)',
+						boxShadow: '0px 0px 8px rgb(255,255,255)'
+					}}
+				>
+					Order
+				</m.button>
 			</Link>
 		</div>
 	)
