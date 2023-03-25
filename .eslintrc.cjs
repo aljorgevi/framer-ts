79044f8bd12d13c6c1de89294f5a15b65d3347a4
aljorgevi <jormencar@yahoo.com>
2023-03-25 11:53:45 +0000

    config eslint and install packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
 		browser: true,
 		es2021: true
 	},
-	extends: ['plugin:react/recommended', 'standard'],
+	extends: ['plugin:react/recommended', 'standard', 'prettier'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaFeatures: {
@@ -11,8 +11,11 @@ module.exports = {
 		},
 		ecmaVersion: 12,
 		sourceType: 'module',
-		project: './tsconfig.json'
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname
 	},
 	plugins: ['react', '@typescript-eslint'],
-	rules: {}
+	rules: {
+		'react/react-in-jsx-scope': 'off'
+	}
 }
